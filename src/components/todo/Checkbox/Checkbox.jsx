92a22c80d70dce5d0719d7fc2452b788e0412617
Juanjo Ortiz <jjortiz@ilernaonline.com>
2021-10-14 15:26:52 +0200

    ✨ checkbox tests started

--- src/components/todo/Checkbox/Checkbox.jsx
+++ src/components/todo/Checkbox/Checkbox.jsx
@@ -3,14 +3,14 @@ import './styles.css'
 
 
 export default function Checkbox({label, value, onChange}){
-   
+    const classCheckbox = value ? 'checked' : 'un-checked'
 
     return (
         <div className='checkbox'>
             <input type={'checkbox'} checked={value} onChange={(e) => 
                                                         onChange({value: e.target.checked, label})}
             /> 
-            <span className='checkbox-label'>{label}</span>
+            <span className={`checkbox-label ${classCheckbox}`}>{label}</span>
         </div>
     )
 }
